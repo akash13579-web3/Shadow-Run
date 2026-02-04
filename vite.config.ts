180fdc24df4bd56ad4632222d4ef6acfd4e9882f
akash13579-web3 <akashneog01010@gmail.com>
2026-02-05 04:31:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADD THIS LINE BELOW
+      base: './', 
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -16,7 +18,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './src'),
         }
       }
     };
